b1ae92d97fee636afe3891ee95f0529d514582e6
José Rui Barros <jose.barros@impactzero.pt>
2019-04-16 14:54:51 +0100

    Added new command line parameter framework and more features.

--- main.py
+++ main.py
@@ -1,46 +1,78 @@
 import requests
 import sys
+import click
 
-# key = "0BE6D41C39906AD3D7E1085F310BBC77"
-key = sys.argv[1]
-# steamid = "76561197990642328"
-steamid = sys.argv[2]
 
-username_payload = {"key": key, "steamids": steamid}
-resp = requests.get(
-    "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002", params=username_payload)
-# print(resp.text)
-username = resp.json()["response"]["players"][0]["personaname"]
+@click.command()
+@click.option('-k', '--key', "key", help="Steam API key required to use the service. This option can not be used in the case there is already one key saved.")
+@click.option('-u', '--user', "steam_id", required=True, help="Steam ID of the user to be searched. Mandatory option.")
+@click.option('-s', '--save', "save", is_flag=True, help="Option switch to save the current key in the app. Will replace an existing one.")
+def leApp(key, steam_id, save):
+    # key = "0BE6D41C39906AD3D7E1085F310BBC77"
+    # key = sys.argv[1]
+    # steam_id = "76561197990642328"
+    # steam_id = sys.argv[2]
 
-gamestats_payload = {"key": key, "steamid": steamid,
-                     "include_played_free_games": 1, "include_appinfo": 1}
-resp = requests.get(
-    "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001", params=gamestats_payload)
-# json = resp.json()
-# print(json)
-sum_mins = 0
-max_mins = 0
-most_played_game = ""
-for game in resp.json()["response"]["games"]:
-    mins = game["playtime_forever"]
-    sum_mins += mins
-    if mins > max_mins:
-        max_mins = mins
-        most_played_game = game["name"]
+    username_payload = {"key": key, "steamids": steam_id}
+    resp = requests.get(
+        "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002", params=username_payload)
+    # print(resp.text)
+    if resp.status_code == 403:
+        print("Invalid Steam API key!")
+        sys.exit()
 
-print(f'''{username} has wasted {sum_mins} minutes, the equivalent of {sum_mins/60:.2f} hours or {sum_mins/60/24:.2f} days 
-of his/her life playing video games on Steam. Mostly by playing {most_played_game} for {max_mins/60:.2f} hours.''')
+    resp.raise_for_status()
 
-recently_payload = {"key": key, "steamid": steamid}
-resp = requests.get(
-    "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001", params=recently_payload)
-response = resp.json()["response"]
+    players_list = resp.json()["response"]["players"]
 
-if response["total_count"] > 0:
+    if len(players_list) == 0:
+        print("No players have been found for that steam ID")
+        sys.exit()
 
-    games_set = set()
-    sum_2week_time = 0
-    for game in response["games"]:
-        games_set.add(game["name"])
-        sum_2week_time += game["playtime_2weeks"]
-    print(f"Has been lately (2 weeks) playing: \n{', '.join(games_set)} for {sum_2week_time/60:.2f} hours.")
+    username = players_list[0]["personaname"]
+
+    gamestats_payload = {"key": key, "steamid": steam_id,
+                         "include_played_free_games": 1, "include_appinfo": 1}
+    resp = requests.get(
+        "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001", params=gamestats_payload)
+
+    resp.raise_for_status()
+
+    # json = resp.json()
+    # print(json)
+    sum_mins = 0
+    max_mins = 0
+    most_played_game = ""
+    for game in resp.json()["response"]["games"]:
+        mins = game["playtime_forever"]
+        sum_mins += mins
+        if mins > max_mins:
+            max_mins = mins
+            most_played_game = game["name"]
+
+    print(f'''{username} has wasted {sum_mins} minutes, the equivalent of {sum_mins/60:.2f} hours or {sum_mins/60/24:.2f} days 
+    of his/her life playing video games on Steam. Mostly by playing {most_played_game} for {max_mins/60:.2f} hours.''')
+
+    recently_payload = {"key": key, "steamid": steam_id}
+    resp = requests.get(
+        "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001", params=recently_payload)
+
+    resp.raise_for_status()
+
+    response = resp.json()["response"]
+
+    if response["total_count"] > 0:
+
+        games_set = set()
+        sum_2week_time = 0
+        for game in response["games"]:
+            games_set.add(game["name"])
+            sum_2week_time += game["playtime_2weeks"]
+        print(
+            f"Has been lately (2 weeks) playing: \n{', '.join(games_set)} for {sum_2week_time/60:.2f} hours.")
+    else:
+        print("Has not been playing anything lately.")
+
+
+if __name__ == '__main__':
+    leApp()
